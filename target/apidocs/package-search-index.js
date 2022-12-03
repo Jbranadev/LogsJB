34a0086fac3526c03d0f581a7024643b9b4390c0
Jose Carlos Bran <jbranadev@gmail.com>
2022-12-03 09:30:57 -0600

    documentación

---
+++ target/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.github.josecarlosbran.LogsJB"},{"l":"io.github.josecarlosbran.LogsJB.Mensajes"},{"l":"io.github.josecarlosbran.LogsJB.Numeracion"}];updateSearchResults();

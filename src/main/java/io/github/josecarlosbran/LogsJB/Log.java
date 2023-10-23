43cf87bf6758d803c10efb8f7ea2d553b57709d3
José Carlos Bran <117953364+Jbranadev@users.noreply.github.com>
2023-10-23 09:32:17 -0600

    Actualización de librería jbsqlutils a version 1.2.2

--- src/main/java/io/github/josecarlosbran/LogsJB/Log.java
+++ src/main/java/io/github/josecarlosbran/LogsJB/Log.java
@@ -6,30 +6,26 @@ import lombok.Setter;
 /**
  * Clase que cumple la función de un POJO para el envío de los Logs a un RestAPI
  */
+@Getter
+@Setter
 public class Log {
 
-    @Getter
-    @Setter
+
     private Integer Id;
 
-    @Getter
-    @Setter
+
     private String Texto;
 
-    @Getter
-    @Setter
+
     private String NivelLog;
 
-    @Getter
-    @Setter
+
     private String Clase;
 
-    @Getter
-    @Setter
+
     private String Metodo;
 
-    @Getter
-    @Setter
+
     private String Fecha;
 
 }
